e787ada374a5e4982c250fe981bfca8d04ca74e8
chaldrich24 <chaldrich24@gmail.com>
2022-03-24 20:30:04 -0400

    create tab and stack navigator for profile

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {AntDesign} from '@expo/vector-icons';
+
+import ProfileScreen from "./screens/ProfileScreen";
+
+const ProfileStack = createNativeStackNavigator();
+
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ title: "My Profile" }}
+      />
+    </ProfileStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <NavigationContainer style={styles.container}>
+    <Tab.Navigator>
+      <Tab.Screen name='ProfileStack' component={ProfileStackScreen} options={{headerShown: false, title: 'Profile', tabBarIcon: () => <AntDesign name="profile" size={22} color='black' />}} />
+    </Tab.Navigator>
+  </NavigationContainer>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#dbd3d8",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
